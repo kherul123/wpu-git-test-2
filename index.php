44464e9876025dbb1d14a81cf446f2b20b89a7c5
kherul123 <kherulhasbiyan@gmail.com>
2024-05-24 20:14:33 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
 <body>
 	<h1>Hello World!</h1>
 	<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem, porro beatae in quod nam ipsam molestiae doloribus reprehenderit culpa repudiandae, similique impedit nisi officia accusamus voluptatum magnam! Rerum, architecto alias.</p>
+	<ul>
+		<li>satu</li>
+		<li>dua</li>
+	</ul>
 </body>
 
 </html>
